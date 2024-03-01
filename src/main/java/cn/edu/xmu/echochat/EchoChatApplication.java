c6cfbe8637c99a851b7c8c92f4caac921da780ff
TingLans <lr030322@163.com>
2024-03-01 13:38:43 +0800

    first commit

---
+++ src/main/java/cn/edu/xmu/echochat/EchoChatApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.xmu.echochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchoChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/xmu/echochat/EchoChatApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.xmu.echochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
