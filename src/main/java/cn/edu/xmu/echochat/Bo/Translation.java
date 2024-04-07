fe92fa4c6696514f345da6e24cdf6dd4c7aabeef
tungwuwt <tungwuwt@gmail.com>
2024-04-07 10:02:13 +0800

    翻译功能

---
+++ src/main/java/cn/edu/xmu/echochat/Bo/Translation.java
@@ -0,0 +1,67 @@
+package cn.edu.xmu.echochat.Bo;
+
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.DigestUtils;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+@Component
+public class Translation {
+    @Value("${spring.Baidu.appid}")
+    private String appid;
+
+    @Value("${spring.Baidu.key}")
+    private String key;
+
+    @Value("${spring.Baidu.url}")
+    private String url;
+
+    public String translate(String content) {
+        Random random = new Random(10);
+        String salt = Integer.toString(random.nextInt());
+        String sign = DigestUtils.md5DigestAsHex((appid + content + salt + key).getBytes());
+
+        String from = "zh";
+        String to = "en";
+
+        MultiValueMap<String, String> paramMap = new LinkedMultiValueMap<>();
+        paramMap.add("q",content);
+        paramMap.add("from", from);
+        paramMap.add("to", to);
+        paramMap.add("appid", appid);
+        paramMap.add("salt",salt);
+        paramMap.add("sign",sign);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(paramMap,headers);
+
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Object> response = restTemplate.postForEntity(url, httpEntity, Object.class);
+
+        String pattern = "dst=([^\\]}]*)";;
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(response.getBody().toString());
+        if (m.find()) {
+            return m.group(1);
+        } else {
+            return "不正确的请求";
+        }
+    }
+}
--- src/main/java/cn/edu/xmu/echochat/ChatCommandLineRunner.java
+++ src/main/java/cn/edu/xmu/echochat/ChatCommandLineRunner.java
@@ -1,16 +1,23 @@
 package cn.edu.xmu.echochat;
 
+import java.util.Random;
 import java.util.Scanner;
 
 import cn.edu.xmu.echochat.Bo.Msg;
 import cn.edu.xmu.echochat.Bo.Sender;
+import cn.edu.xmu.echochat.Bo.Translation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 
 @Component
 public class ChatCommandLineRunner implements CommandLineRunner {
 
     private final Sender sender;
+
+    @Autowired
+    Translation translation;
 
     // 构造器注入Sender
     public ChatCommandLineRunner(Sender sender) {
@@ -32,12 +39,19 @@ public class ChatCommandLineRunner implements CommandLineRunner {
                     System.out.println("请输入消息内容:");
                     String content = scanner.nextLine();
                     msg.setContent(content);
-                } else {
+                    sender.sendQueue(msg); // 或者 sender.sendTopic(msg); 根据实际情况调用
+                } else if (messageType == 1){
                     // 对于文件类型的消息，可以在这里添加逻辑处理
                     System.out.println("文件消息暂不支持通过命令行发送。");
-                }
+                } else if (messageType == 2) {
+                    // 文本翻译
+                    System.out.println("请输入需要翻译内容:");
+                    String content = scanner.nextLine();
 
-                sender.sendQueue(msg); // 或者 sender.sendTopic(msg); 根据实际情况调用
+                    String to = translation.translate(content);
+
+                    System.out.println(to);
+                }
             }
         }
     }
