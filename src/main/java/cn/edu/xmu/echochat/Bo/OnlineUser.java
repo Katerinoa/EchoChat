adc998686e37c1427c166fba2d65a01a5161e880
tungwuwt <tungwuwt@gmail.com>
2024-04-22 17:40:40 +0800

    debug 消息接受成功

--- src/main/java/cn/edu/xmu/echochat/Bo/OnlineUser.java
+++ src/main/java/cn/edu/xmu/echochat/Bo/OnlineUser.java
@@ -1,13 +1,20 @@
 package cn.edu.xmu.echochat.Bo;
 
 import cn.edu.xmu.echochat.config.ActiveMQConfig;
+import cn.edu.xmu.echochat.config.CustomMessageConverter;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.jms.Destination;
 import jakarta.jms.Queue;
 import jakarta.websocket.Session;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.config.SimpleJmsListenerEndpoint;
+import org.springframework.jms.config.JmsListenerEndpointRegistry;
 import org.springframework.jms.core.JmsMessagingTemplate;
 
 import java.io.IOException;
@@ -15,6 +22,9 @@ import java.io.IOException;
 @Slf4j
 @ConditionalOnProperty(prefix = "spring.activemq.jms", name = "enable", havingValue = "true")
 public class OnlineUser {
+
+    private JmsListenerEndpointRegistry registry;
+    JmsListenerContainerFactory<?> factory;
 
     private JmsMessagingTemplate jmsMessagingTemplate;
 
@@ -25,11 +35,33 @@ public class OnlineUser {
     private String queueName;
     private ObjectMapper objectMapper;
 
-    public OnlineUser(Session session, Long userId, JmsMessagingTemplate jmsMessagingTemplate) {
+    public OnlineUser(Session session, Long userId, JmsMessagingTemplate jmsMessagingTemplate, JmsListenerContainerFactory<?> factory) {
         this.session = session;
         this.queueName = 'u' + userId.toString();
         this.queue = ActiveMQConfig.queue(queueName);
         this.jmsMessagingTemplate = jmsMessagingTemplate;
+        this.factory = factory;
+        addQueueListener();
+    }
+
+    private void addQueueListener() {
+        CustomMessageConverter customMessageConverter = new CustomMessageConverter();
+        registry = new JmsListenerEndpointRegistry();
+        SimpleJmsListenerEndpoint endpoint = new SimpleJmsListenerEndpoint();
+        endpoint.setId(queueName + "Endpoint");
+        endpoint.setDestination(queueName);
+        endpoint.setMessageListener(message -> {
+            try {
+                Msg msg = (Msg) customMessageConverter.fromMessage(message);
+                if (msg instanceof Msg) {   // 根据实际情况进行类型转换和判断
+                    readActiveQueue(msg);
+                }
+            } catch (Exception e) {
+                log.error("处理消息时发生错误", e);
+            }
+        });
+
+        registry.registerListenerContainer(endpoint, factory, true);
     }
 
     public void sendQueue(Long receiverId, Msg message) {
@@ -41,9 +73,9 @@ public class OnlineUser {
         jmsMessagingTemplate.convertAndSend(destination, message);
     }
 
-    @JmsListener(destination = "#{onlineUser.getQueueName()}", containerFactory = "queueListener")
     public void readActiveQueue(Msg message) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         session.getBasicRemote().sendText(objectMapper.writeValueAsString(message));
     }
 
--- src/main/java/cn/edu/xmu/echochat/ws/WebsocketServer.java
+++ src/main/java/cn/edu/xmu/echochat/ws/WebsocketServer.java
@@ -1,5 +1,6 @@
 package cn.edu.xmu.echochat.ws;
 
+import cn.edu.xmu.echochat.Bo.ApplicationContextProvider;
 import cn.edu.xmu.echochat.Bo.Msg;
 import cn.edu.xmu.echochat.Bo.OnlineUser;
 import cn.edu.xmu.echochat.Bo.User;
@@ -18,6 +19,8 @@ import jakarta.websocket.server.ServerEndpoint;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.connection.SingleConnectionFactory;
 import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.stereotype.Component;
@@ -29,6 +32,7 @@ import java.io.IOException;
 @Component
 public class WebsocketServer {
     private JmsMessagingTemplate jmsMessagingTemplate;
+    private ApplicationContext context;
 
     private Session session;
     private String username;
@@ -44,11 +48,12 @@ public class WebsocketServer {
         this.password = password;
         this.session = session;
         this.jmsMessagingTemplate = SpringContextUtil.getBean(JmsMessagingTemplate.class);
+        context = ApplicationContextProvider.getContext();
 
         User user = this.userPoMapper.findByUsernameAndPassword(username, password);
         if (user != null) {
             log.info("login success: " + this.username);
-            this.onlineUser = new OnlineUser(session, user.getId(), jmsMessagingTemplate);
+            this.onlineUser = new OnlineUser(session, user.getId(), jmsMessagingTemplate, (JmsListenerContainerFactory<?>) context.getBean("queueListener"));
         } else {
             log.info("failed matching: " + this.username);
             session.close();
